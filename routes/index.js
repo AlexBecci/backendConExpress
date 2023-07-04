6c01adb67278e6e25fa38ff14893465a0fc840b2
Alex <beccialex2002@gmail.com>
2023-07-04 15:52:35 -0300

    separando las responsabilidades con express.Router

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+const productsRouter = require('./productsRouter');
+const categoriesRouter = require('./categoriesRouter');
+const usersRouter = require('./usersRouter')
+
+
+
+
+function routerApi(app) {
+  app.use('/products', productsRouter)
+  app.use('/categories', categoriesRouter)
+  app.use('/users', usersRouter)
+
+
+}
+
+module.exports = routerApi;
