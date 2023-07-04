57f87b2d363aca3bca9b64a5c3cc20f72d8a8709
Alex <beccialex2002@gmail.com>
2023-07-04 14:40:02 -0300

    init project

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+const port = 3000;
+
+
+app.get('/', (req, res) => {
+  res.send('Hola mi server en express')
+})
+
+app.listen(port, () => {
+  console.log("Estamos corriendo en el puerto.." + port)
+})
+
