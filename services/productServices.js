8a6dcbc5d7d19523fb087dc6e1f8e13ee7fde797
Alex <beccialex2002@gmail.com>
2023-07-20 13:58:30 -0300

    Async/Await and try/catch

--- services/productServices.js
+++ services/productServices.js
@@ -18,7 +18,7 @@ class ProductServices {
     }
   }
 
-  create(data) {
+  async create(data) {
     const newProduct = {
       id: faker.datatype.uuid(),
       ...data
@@ -26,13 +26,18 @@ class ProductServices {
     this.products.push(newProduct);
     return newProduct;
   }
-  find() {
-    return this.products;
+  async find() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(this.products)
+      }, 5000)
+    })
   }
-  findOne(id) {
+  async findOne(id) {
     return this.products.find(item => item.id === id)
   }
-  update(id, changes) {
+
+  async update(id, changes) {
     const index = this.products.findIndex(item => item.id === id);
     if (index === -1) {
       throw new Error("product not found");
@@ -45,7 +50,7 @@ class ProductServices {
 
 
   }
-  delete(id) {
+  async delete(id) {
     const index = this.products.findIndex(item => item.id === id);
     if (index === -1) {
       throw new Error("product not found");
